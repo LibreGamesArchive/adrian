e1a4f526e32b7976ce45e561361098203084813d
chbhanukalyan <chbhanukalyan@3f8066fa-308d-11de-bd8d-4b29057232bf>
2009-05-10 08:33:16 +0000

    Basic static model loading code

--- src/staticloader.cpp
+++ src/staticloader.cpp
@@ -1,8 +1,22 @@
 #include "staticloader.h"
 
-void LoadStaticModel(const char *fn)
+#include <GL/gl.h>
+#include <GL/glu.h>
+
+SModel::SModel(void)
 {
-	const aiScene* scene = aiImportFile(fn,
+	scene = NULL;
+}
+
+SModel::~SModel()
+{
+	if (scene)
+		aiReleaseImport(scene);
+}
+
+int SModel::Load(const char *fn)
+{
+	scene = aiImportFile(fn,
 						aiProcess_CalcTangentSpace       | 
 						aiProcess_Triangulate            |
 						aiProcess_JoinIdenticalVertices  |
@@ -13,10 +27,38 @@ void LoadStaticModel(const char *fn)
 		exit(0);
 	}
 
-	/* Convert it into a Display List of whatever */
-//	glBegin(GL_TRIANGLE_STRIP);
-//	glEnd();
- 
-	aiReleaseImport(scene);
+	return 0;
+}
+
+void SModel::Render(void)
+{
+static int ass = 0;
+ass++;
+
+	glPushMatrix();
+	glTranslatef(x, y, z);
+	glRotatef(-angle, 1, 0, 0);
+
+	glDisable(GL_TEXTURE_2D);
+	for (int i = 0; i < (int)scene->mNumMeshes; i++) {
+		const aiMesh *mesh = scene->mMeshes[i];
+//		const aiMaterial *mat = scene->mMaterials[mesh->mMaterialIndex];
+		glColor3f(1-(i/18.0),1-(i/18.0),1);
+
+		glBegin(GL_TRIANGLE_FAN);
+			for (int j = 0; j < (int)mesh->mNumFaces; j++) {
+				const aiFace *face = &mesh->mFaces[j];
+				const aiVector3D *v = mesh->mVertices;
+
+				for (int k = 0; k < (int)face->mNumIndices; k++) {
+					unsigned int idx = face->mIndices[k];
+					glVertex3f(v[idx].x, v[idx].y, v[idx].z);
+					if (ass == 1) printf("(%3d %3d %3d)%f %f %f\n", i, j, k, v[idx].x, v[idx].y, v[idx].z);
+				}
+			}
+		glEnd();
+	}
+
+	glPopMatrix();
 }
 
